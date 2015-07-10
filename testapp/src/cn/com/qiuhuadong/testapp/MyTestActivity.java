791857403789389eefa5edad25a3c9cc3be6ecf1
qiuhuadong2015 <qiuhuadong2014@hotmail.com>
2015-07-10 09:30:06 +0800

    test

---
+++ testapp/src/cn/com/qiuhuadong/testapp/MyTestActivity.java
@@ -0,0 +1,15 @@
+package cn.com.qiuhuadong.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyTestActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
